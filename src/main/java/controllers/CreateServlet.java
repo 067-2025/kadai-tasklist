c4e64b7586accd86ac1c16ba91726c8b908638ed
RINA.HONDA <RINA.HONDA@grp.tmnf.jp>
2025-05-28 15:30:37 +0900

    バリデーションを追加

--- src/main/java/controllers/CreateServlet.java
+++ src/main/java/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import jakarta.persistence.EntityManager;
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,6 +13,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import models.Content;
+import models.validators.MessageValidator;
 import utils.DBUtil;
 
 /**
@@ -47,13 +50,29 @@ public class CreateServlet extends HttpServlet {
             c.setCreated_at(currentTime);
             c.setUpdated_at(currentTime);
 
-            em.persist(c);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
+            // バリデーションを実行してエラーがあったら新規登録のフォームに戻る
+            List<String> errors = MessageValidator.validate(c);
+            if (errors.size() > 0) {
+                em.close();
 
-            response.sendRedirect(request.getContextPath() + "/index");
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("content", c);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/contents/new.jsp");
+                rd.forward(request, response);
+            } else {
+                // データベースに保存
+                em.persist(c);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+                em.close();
+
+                // indexのページにリダイレクト
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
+
         }
-
     }
 }
---
+++ src/main/java/models/validators/MessageValidator.java
@@ -0,0 +1,29 @@
+package models.validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Content;
+
+public class MessageValidator {
+    // バリデーションを実行する
+    public static List<String> validate(Content c) {
+        List<String> errors = new ArrayList<String>();
+
+        String content_error = validateContent(c.getContent());
+        if (!content_error.equals("")) {
+            errors.add(content_error);
+        }
+
+        return errors;
+    }
+
+    // メッセージの必須入力チェック
+    private static String validateContent(String content) {
+        if (content == null || content.equals("")) {
+            return "タスクを入力してください。";
+        }
+
+        return "";
+    }
+}
