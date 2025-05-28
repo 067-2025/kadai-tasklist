a68e313e443974dcc0febd47822e196b37ddfc90
RINA.HONDA <RINA.HONDA@grp.tmnf.jp>
2025-05-28 14:18:07 +0900

    EditServlet.javaを作成

---
+++ src/main/java/controllers/EditServlet.java
@@ -0,0 +1,54 @@
+package controllers;
+
+import java.io.IOException;
+
+import jakarta.persistence.EntityManager;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import models.Content;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class EditServlet
+ */
+@WebServlet("/edit")
+public class EditServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EditServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        // 該当のIDのメッセージ1件のみをデータベースから取得
+        Content c = em.find(Content.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        // メッセージ情報とセッションIDをリクエストスコープに登録
+        request.setAttribute("content", c);
+        request.setAttribute("_token", request.getSession().getId());
+
+        // メッセージIDをセッションスコープに登録
+        request.getSession().setAttribute("content_id", c.getId());
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/contents/edit.jsp");
+        rd.forward(request, response);
+    }
+
+}
